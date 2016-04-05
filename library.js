8e2777c9c8c92937f32144ccbebc3269f77b44d8
Jason <jsmylnycky@peachworks.com>
2016-04-05 19:50:24 -0400

    update errors

--- library.js
+++ library.js
@@ -72,7 +72,7 @@
 
 				passportOAuth.Strategy.prototype.userProfile = function(token, secret, params, done) {
 					this._oauth.get(constants.userRoute, token, secret, function(err, body, res) {
-						if (err) { return done(new Error('failed to fetch user profile', err)); }
+						if (err) { console.log('userProfile 1 error', err); return done(new Error(err)); }
 
 						try {
 							var json = JSON.parse(body);
@@ -94,7 +94,7 @@
 				passportOAuth.Strategy.prototype.userProfile = function(accessToken, done) {
 					var _this = this;
 					return _this._oauth2.get(constants.userIdRoute, accessToken, function(err, body, res) {
-						if (err) { return done(new Error('failed to fetch user id', err)); }
+						if (err) { console.log('userProfile 2 error', err); return done(new Error(err)); }
 
 						var idJson = {};
 						try {
@@ -104,7 +104,7 @@
 						}
 
 						return _this._oauth2.get(constants.userBattletagRoute, accessToken, function(err, body, res) {
-							if (err) { return done(new Error('failed to fetch user battletag', err)); }
+							if (err) { console.log('battletag route error', err); return done(new Error(err)); }
 
 							var battletagJson = {};
 							try {
@@ -114,7 +114,7 @@
 							}
 
 							return _this._oauth2.get(constants.userCharactersRoute, accessToken, function(err, body, res) {
-								if (err) { return done(new Error('failed to fetch user battletag', err)); }
+								if (err) { console.log('characterroute error', err); return done(new Error(err)); }
 
 								var charactersJson = {};
 								try {
